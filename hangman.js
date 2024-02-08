b8c0c5ce5a06ff3e1bf3c790ff53a380d7c4ace2
Bestindom <bestindom@gmail.com>
2024-02-08 17:47:03 +0100

    Update day 8

--- hangman.js
+++ hangman.js
@@ -20,7 +20,8 @@ let draw = document.getElementById("draw");
 let lettersWord = document.getElementById("lettersWord");
 let wordContainer = document.getElementById("wordContainer");
 let alphabet = document.getElementById("alphabet");
-let youLive = 8;
+let img = document.getElementById('image');
+let yourLive = 1;
 
 // Generate a random integer between min (inclusive) and max (inclusive)
 function getRandomInt(min, max)
@@ -66,17 +67,20 @@ function printAlphabet (wordToPlay)
 
     for (let i = 0; i < alphabetList.length; i++)
     {
-        let letter = document.createElement("div");
+        let letter = document.createElement("button");
         letter.classList.add("col-sm-2");
-        letter.id = 'letter';  //mirar el tema id, PARA QUE CADA UNA TENGA UNO DIFETENTE Y HACER....
+        letter.id = 'letter' + i;  //mirar el tema id, PARA QUE CADA UNA TENGA UNO DIFETENTE Y HACER....
         letter.textContent = alphabetList[i];
 
         row.appendChild(letter);
 
         letter.addEventListener("click",
-        function()
+        function clickHandler()
         {
             checkLetter(letter, wordToPlay);
+
+            // elimino el event listener de esa letra, NOTA PARA LA PROXIMA, NOMBRAR LA FUNCION :)
+            letter.removeEventListener("click", clickHandler);
         });
     }
     alphabet.appendChild(row);
@@ -86,6 +90,7 @@ function checkLetter(letter, wordToPlay)
 {
     console.log(letter.textContent);
     console.log(wordToPlay);
+    let concide = false;
 
     for (let i = 0; i < wordToPlay.length; i++)
     {
@@ -95,14 +100,35 @@ function checkLetter(letter, wordToPlay)
             console.log("LA LETRA COINCIDE");
             let letterSwap = document.getElementById("underscore" + i)
             letterSwap.textContent = wordToPlay[i];
+            concide = true;
         }
+    }
+
+    if (!concide)
+    {
+        letter.style.background = 'crimson';
+        liveControl();
+    }
+    else
+    {
+        letter.style.background = 'seagreen';
     }
 };
 
-function winOrLose ()
+function liveControl()
 {
-    if (youLive < 0)
+    for (let i = 0; i < 9; i++)
     {
-        
+        if(i == yourLive)
+        {
+            img.src = '../images/step' + i + '.png';
+        }
     }
+
+    if(yourLive == 8)
+    {
+        alert('Dead');
+    }
+
+    yourLive++;
 };
